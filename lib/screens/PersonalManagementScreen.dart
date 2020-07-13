3530db955ad75d51608f62797364ec5b34cd6789
Burning Bush Music <BurningBushMusic@Newlifes-iMac.local>
2020-07-12 19:06:08 -0500

    completed the styling of the first section of Personal Management Screen

--- lib/screens/PersonalManagementScreen.dart
+++ lib/screens/PersonalManagementScreen.dart
@@ -30,28 +30,162 @@ class _PersonalManagementScreen extends State<PersonalManagementScreen> {
           backgroundColor: Theme.of(context).canvasColor,
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height -
-                    MediaQuery.of(context).padding.top,
-                child: Padding(padding: EdgeInsets.all(8.0), child: Column(children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: <Widget>[],),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: <Widget>[],),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: <Widget>[],),
-                  ),
-                ],),),
+          child: Column(children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              FlatButton(
+                                  onPressed: null,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    child: Image.network(
+                                      "https://photo.sohu.com/88/60/Img214056088.jpg",
+                                      fit: BoxFit.cover,
+                                      height: 60.0,
+                                      width: 60.0,
+                                    ),
+                                  )),
+                              Text(
+                                "你好，凯瑟琳",
+                                style: Theme.of(context).textTheme.headline1,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 30.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                      color: Color.fromARGB(255, 255, 235, 133),
+                                      width: 8,
+                                      height: 50,
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 16.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text("本月累计阅读时间5小时23分钟",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText2,
+                                              textAlign: TextAlign.left),
+                                          SizedBox(height: 8.0),
+                                          Text("好友排名第17名",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText2,
+                                              textAlign: TextAlign.left),
+                                        ],
+                                      ),
+                                    ),
+                                    FlatButton(
+                                        textColor:
+                                            Color.fromARGB(255, 7, 59, 76),
+                                        onPressed: null,
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 10),
+                                          child: Container(
+                                              height: 30,
+                                              width: 110,
+                                              decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(60),
+                                                color: Color.fromARGB(
+                                                    255, 240, 240, 240),
+                                              ),
+                                              child: Center(
+                                                child: Text(
+                                                  "我的个人名片",
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyText2,
+                                                ),
+                                              )),
+                                        )),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Row(
+                                    children: <Widget>[
+                                      FlatButton(
+                                        onPressed: null,
+                                        child: Ink(
+                                          decoration: const ShapeDecoration(
+                                            color: Color.fromARGB(
+                                                255, 168, 218, 220),
+                                            shape: CircleBorder(),
+                                          ),
+                                          child: IconButton(
+                                              color: Color.fromARGB(
+                                                  255, 255, 255, 255),
+                                              iconSize: 25,
+                                              icon: Icon(Icons.person),
+                                              tooltip: '我的好友',
+                                              onPressed: null),
+                                        ),
+                                      ),
+                                      Text("我的好友"),
+                                      SizedBox(
+                                        width: 15,
+                                      ),
+                                      Text("156个关注"),
+                                    ],
+                                  ),
+                                  height: 60,
+                                  width: 210,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(23),
+                                    color: Color.fromARGB(255, 240, 240, 240),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ]
-          ),
+            ),
+          ]),
         ));
   }
 }
